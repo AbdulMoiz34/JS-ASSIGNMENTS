d609d7c3865cf21745029fb31d838f502316906d
Abdul Moiz <moiz05366@gmail.com>
2024-12-17 03:41:14 +0500

    added task#01

---
+++ task#01/app.js
@@ -0,0 +1,94 @@
+//                       ASSIGNMENT NO: 1
+// QUESTION 1
+
+// alert("welcome! dear");
+
+// QUESTION 2
+
+// alert("Error! Please Enter a valid password.");
+
+// QUESTION 3
+
+// alert("Welcome to JS Land \nHappy Coding School!");
+
+// QUESTION 4
+
+// alert("Welcome to JS Land");
+// alert("Happy Coding!");
+
+// QUESTION 5
+
+// console.log("Hello... I can run JS through my web browser's console");
+
+
+//                       ASSIGNMENT NO: 2
+
+// QUESTION 1
+
+//  const username;
+
+// QUESTION 2
+
+// const myName = "Abdul Moiz";
+
+// QUESTION 3
+
+// const message = "Hello World!";
+// alert(message);
+
+
+// QUESTION 4
+
+// const studentName = "Mohammad Umar!";
+// const studentAge = 20;
+// const studentCourse = "web and mobile app development";
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCourse);
+
+// QUESTION 5
+
+// const display = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(display);
+
+
+// QUESTION 6
+
+// const email = "moiz05366@gmail.com";
+// alert(email);
+
+// QUESTION 7
+
+// const book = "A smarter way to learn javascript";
+// alert(book);
+
+// QUESTION 8
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// const str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(str);
+
+
+//                     ASSIGNMENT NO: 3
+
+// QUESTION 1
+
+// const age = 20;
+// alert(age);
+
+// QUESTION 2
+
+// QUESTION 3
+
+// const birthYear = 2004;
+// document.write("My birth year is " + birthYear);
+// document.write("<br/> Data type of declared variable is number.");
+
+
+// QUESTION 4
+
+// const visitorName = "Ahmed Raza";
+// const productTitle = "5-T shirt";
+// const quantity = 2;
+// document.write(visitorName + " ordered " + productTitle + " on moiz clothing store.");
