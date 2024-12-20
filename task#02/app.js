3beca42af9b3c9f313212747c4721ae97be3ef26
Abdul Moiz <moiz05366@gmail.com>
2024-12-20 16:15:07 +0500

    added chap4

---
+++ task#02/app.js
@@ -0,0 +1,30 @@
+//            ASSIGNMENT 4
+// QUESTION 1
+// let str1, str2, str3;
+
+
+// QUESTION 2
+// LEGAL VARIABLE NAME
+// let firstName;  // camelCase
+// let _username;  // underscore are allowed at the beginning
+// let age;  // simple and valid variable name
+// let user2; // numbers are allowed, but not at the beginning
+// let $price // dollar sign is allowed at the beginning
+
+// ILLEGAL VARIABLE NAME
+// let 1num;  variable must be begin with alphabets or special numbers e.g. $_
+// let num-1; must not be dashed in the name
+// let num 1  space isn't allowed
+// let var;    name shouldn't be reserved words
+// let @num;   special characters are not allowed
+
+// QUESTION 3
+// document.write(`
+//     <h1>Rules for naming JS Variables</h1>
+//     <ul>
+//         <li>Variable must begin with a letter, $ or _. e.g. name, $name or _name</li>
+//         <li>Variable names can only contain numbers, $, _</li>
+//         <li>Variable names are case sensitive</li>
+//         <li>Variable names should not be JS keywords</li>
+//     </ul>
+//     `);
